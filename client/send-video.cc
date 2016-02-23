ffb35428c175594842b7519c6d6999e19cd64bc6
Henner Zeller <h.zeller@acm.org>
2016-02-22 19:23:45 -0800

    o revert old-ubuntu distro hack.

--- client/send-video.cc
+++ client/send-video.cc
@@ -138,7 +138,7 @@ int main(int argc, char *argv[]) {
 
     // Get a pointer to the codec context for the video stream
     pCodecCtxOrig = pFormatCtx->streams[videoStream]->codec;
-    double fps = -1;//av_q2d(pFormatCtx->streams[videoStream]->r_frame_rate);
+    double fps = av_q2d(pFormatCtx->streams[videoStream]->r_frame_rate);
     if (fps < 0) {
         fps = av_q2d(pFormatCtx->streams[videoStream]->avg_frame_rate);
     }
