fd9a506a281d79aa7c072c69f4da6f8ac2eab1af
Leonardo Romor <leonardo.romor@gmail.com>
2016-04-25 03:15:58 +0200

    added pong client

--- client/pong-client.cc
+++ client/pong-client.cc
@@ -13,7 +13,10 @@
 // along with this program.  If not, see <http://gnu.org/licenses/gpl-2.0.txt>
 
 #include <linux/joystick.h>
+#include <assert.h>
 #include <iostream>
+#include <netdb.h>
+#include <sys/socket.h>
 #include <stdio.h>
 #include <stdint.h>
 #include <sys/types.h>
@@ -29,72 +32,145 @@
 #define JS_EVENT_AXIS 0x02
 #define JS_EVENT_INIT 0x80
 
+//Speed factor for now hardcoded
+#define VFACTOR 1e5
+
+class Timer {
+public:
+    Timer() : start_(0) {}
+
+    void Start() {
+        struct timeval tp;
+        gettimeofday(&tp, NULL);
+        start_ = tp.tv_sec * 1000000 + tp.tv_usec;
+    }
+
+    float GetElapsedInMilliseconds() {
+        struct timeval tp;
+        gettimeofday(&tp, NULL);
+        return (tp.tv_sec * 1000000 + tp.tv_usec - start_) / 1e3;
+    }
+
+    inline void clear() { start_ = 0; }
+
+private:
+    int64_t start_;
+};
+
+int UDPClient(const char *host, const char *port) {
+    struct addrinfo addr_hints = {0};
+    addr_hints.ai_family = AF_INET;
+    addr_hints.ai_socktype = SOCK_DGRAM;
+
+    struct addrinfo *addr_result = NULL;
+    int rc;
+    if ((rc = getaddrinfo(host, port, &addr_hints, &addr_result)) != 0) {
+        fprintf(stderr, "Resolving '%s': %s\n", host, gai_strerror(rc));
+        return -1;
+    }
+    if (addr_result == NULL)
+        return -1;
+    int fd = socket(addr_result->ai_family,
+                    addr_result->ai_socktype,
+                    addr_result->ai_protocol);
+    if (fd >= 0 &&
+        connect(fd, addr_result->ai_addr, addr_result->ai_addrlen) < 0) {
+        perror("connect()");
+        close(fd);
+        fd = -1;
+    }
+
+    freeaddrinfo(addr_result);
+    return fd;
+}
+
 class JoyStick {
 public:
     class EventListener {
     public:
-        virtual void button_pressed(const int button, const bool status) = 0;
+        virtual void button_pressed(const int button, const int status) = 0;
         virtual void axis_moving(const int axis, const int value) = 0;
     };
 
     JoyStick(const char *js, EventListener &handler);
     ~JoyStick() { close(fd_); }
-    void Start();
+    void WaitEvent( const float ms = 1);
 private:
     const int fd_;
     EventListener &handler_;
     JoyStick();
 };
-//Need select
-//read (fd, &e, sizeof(e));
+
 JoyStick::JoyStick(const char *js, EventListener &handler) : fd_(open(js, O_RDONLY)), handler_(handler){
-    if(fd_ < 0) {
+    if (fd_ < 0) {
         std::cerr << "Error opening joystick" << std::endl;
         exit(0);
     }
 }
 
-void JoyStick::Start() {
+void JoyStick::WaitEvent(const float ms) {
+    assert(ms >= 0);
+    const unsigned int us = ms * 1000;
     // Select stuff
     js_event event;
     fd_set rfds;
     struct timeval tv;
     int retval;
-
-    for (;;) {
-        FD_ZERO(&rfds);
-        FD_SET(this->fd_, &rfds);
-        tv.tv_sec = 1;
-        tv.tv_usec = 0;
-        retval = select(fd_ + 1, &rfds, NULL, NULL, &tv);
-        if (retval == -1)
-            std::cerr << "Error on select" << std::endl;
-        else if (retval) {
-            read(fd_, &event, sizeof(event));
-            switch (event.type) {
-            case JS_EVENT_BUTTON:
-                handler_.button_pressed(event.number,event.value);
-                break;
-            case JS_EVENT_AXIS:
-                handler_.axis_moving(event.number,(bool)event.value);
-                break;
-            }
+    FD_ZERO(&rfds);
+    FD_SET(this->fd_, &rfds);
+    tv.tv_sec = 0;
+    tv.tv_usec = us;
+    retval = select(fd_ + 1, &rfds, NULL, NULL, &tv);
+    if (retval == -1)
+        std::cerr << "Error on select" << std::endl;
+    else if (retval) {
+        read(fd_, &event, sizeof(event));
+        switch (event.type) {
+        case JS_EVENT_BUTTON:
+            handler_.button_pressed(event.number, event.value);
+            break;
+        case JS_EVENT_AXIS:
+            handler_.axis_moving(event.number, event.value);
+            break;
         }
     }
 }
 
-class EventHandler : public JoyStick::EventListener {
+//For now just transmitter (we could add vibration if supported when a player lose)
+class PongClient : public JoyStick::EventListener {
 public:
-    virtual void button_pressed(const int button, const bool status);
+    PongClient(const char *host, const char *port);
+    void Sim(const float dt);
+    void Send();
+    virtual void button_pressed(const int button, const int status) {}
     virtual void axis_moving(const int axis, const int value);
+
+private:
+    const int udp_fd_;
+    int16_t pos_;
+    int16_t speed_;
 };
 
-void EventHandler::button_pressed(const int button, const bool status){
-    std::cout << "button: " << button << " status: " << status << std::endl;
+PongClient::PongClient(const char *host, const char *port) :
+    udp_fd_(UDPClient(host, port)), pos_(0) {
+    if (udp_fd_ < 0) {
+        std::cerr << "Error on opening udp socket" << std::endl;
+        exit(0);
+    }
 }
 
-void EventHandler::axis_moving(const int axis, const int value){
-    std::cout << "axis: " << axis << " value: " << value << std::endl;
+void PongClient::axis_moving(const int axis, const int value){
+    if(axis == 1){
+        speed_ = value / VFACTOR;
+    }
+}
+
+void PongClient::Sim(const float dt) {
+    pos_ += speed_ * dt;
+}
+
+void PongClient::Send() {
+    if (write(udp_fd_, &pos_, sizeof(pos_)) < 0) return;
 }
 
 int main(int argc, char *argv[]){
@@ -103,9 +179,17 @@ int main(int argc, char *argv[]){
         js_file = argv[1];
     }
 
-    EventHandler event_handler = EventHandler();
-    JoyStick js(js_file, event_handler);
-    js.Start();
+    PongClient pong_client = PongClient("localhost", "10000");
+    JoyStick js(js_file, pong_client);
+    Timer t;
+    float dt = 0;
+    for (;;) {
+        t.Start();
+        js.WaitEvent();
+        pong_client.Sim(dt);
+        pong_client.Send();
+        dt = t.GetElapsedInMilliseconds();
+    }
 
     return 0;
 }
