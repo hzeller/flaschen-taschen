c1500da3c90e4b2daf3d082bad58005f320db3f6
Scotty Allen <scotty@scottyallen.com>
2016-05-22 13:50:41 -0700

    Correctly set emacs settings.

--- client/flaschen.py
+++ client/flaschen.py
@@ -1,4 +1,4 @@
-# -*- mode: c++; c-basic-offset: 4; indent-tabs-mode: nil; -*-
+# -*- mode: python; c-basic-offset: 2; indent-tabs-mode: nil; -*-
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
