41d7045cbef338f1618d539257a7c1b7a8b3c643
Scotty Allen <scotty@scottyallen.com>
2016-05-22 13:34:05 -0700

    Use width and height in the example.

--- client/simple-example.py
+++ client/simple-example.py
@@ -7,7 +7,7 @@ ft = flaschen.Flaschen(UDP_IP, UDP_PORT, 45, 35)
 
 while True:
   for b in xrange(0, 256):
-    for y in xrange(0, 35):
-      for x in xrange(0, 45):
+    for y in xrange(0, ft.height):
+      for x in xrange(0, ft.width):
         ft.set(x, y, ((x * 255) / 45, (y * 255) / 35, b))
     ft.send()
