7dbc9e4c527fbd8f2b222ffff960682d237d604c
Gareth Morgan <gareth@axum.graphics>
2017-06-01 13:52:10 -0700

    Remove the right bloody file

---
+++ examples-api-use/simple-example.py
@@ -0,0 +1,14 @@
+# (Hey Scotty, how can I set the 'include' path for python ?)
+import flaschen
+
+UDP_IP = 'localhost'
+UDP_PORT = 1337
+
+ft = flaschen.Flaschen(UDP_IP, UDP_PORT, 45, 35)
+
+while True:
+  for b in xrange(0, 256):
+    for y in xrange(0, ft.height):
+      for x in xrange(0, ft.width):
+        ft.set(x, y, ((x * 255) / 45, (y * 255) / 35, b))
+    ft.send()
