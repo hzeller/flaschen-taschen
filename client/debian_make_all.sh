d7d663ce80c5ff2ea00c96372e24e16f210e7c6b
Henner Zeller <h.zeller@acm.org>
2019-07-24 11:10:25 -0700

    Use canonical version of libgraphicsmagic dev

--- client/debian_make_all.sh
+++ client/debian_make_all.sh
@@ -3,5 +3,5 @@
 set -euo pipefail
 
 make clean
-sudo apt-get install -y build-essential libgraphicsmagick++1-dev libavcodec-dev libavformat-dev libswscale-dev
+sudo apt-get install -y build-essential libgraphicsmagick++-dev libavcodec-dev libavformat-dev libswscale-dev
 make -k send-text send-image send-video
